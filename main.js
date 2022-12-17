5f4a3a0b7058f0661cd30080bdfeef4a01cc1ea6
Mahbub-Hridoy <90470946+Mahbub-Hridoy@users.noreply.github.com>
2022-12-17 23:41:37 +0600

    new add file

--- main.js
+++ main.js
@@ -29,9 +29,9 @@ x=prompt('enter your name');
 document.write(x);*/
 
 
-var a = 20;
+/*var a = 20;
 var b = 30;
 console.log(a+b);
 var c = 40;
 var d = 60;
-console.log(c+d);
+console.log(c+d);*/
---
+++ test.js
@@ -0,0 +1,11 @@
+/*var a;
+var b;
+a=20;
+b=a;
+document.write(a+b);
+document.write(b-a);
+console.log(b-a);
+
+console.log(a+b);*/
+
+
