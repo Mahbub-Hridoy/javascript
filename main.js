abb5c9b977d3a394c0753f9b1cf931bce85e7f21
Mahbub-Hridoy <90470946+Mahbub-Hridoy@users.noreply.github.com>
2022-12-17 20:54:19 +0600

    update

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
 //javascript output part
-document.write('mahbubur rahman');
+/*document.write('mahbubur rahman');
 
 document.getElementById('h1').innerHTML = 'mahbubur rahman';
 
 window.alert('hey mahbub'); 
 
 
-console.log('mahbub');
+console.log('mahbub');*/
 
 
 
 //use input part
 
-var x;
+/*var x;
 
 x=prompt('enter your name');
 
 document.write(x);
-console.log(x);
+console.log(x);*/
 
 
 /*var x;
@@ -29,3 +29,9 @@ x=prompt('enter your name');
 document.write(x);*/
 
 
+var a = 20;
+var b = 30;
+console.log(a+b);
+var c = 40;
+var d = 60;
+console.log(c+d);
